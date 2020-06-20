96fce28786f53f43f7087029f29cd9de218b53cd
Stas <nevmyvakastas@gmail.com>
2020-06-21 02:30:20 +0600

    installed redux, added inputHandler

--- src/Components/PostForm.js
+++ src/Components/PostForm.js
@@ -3,11 +3,30 @@ import React from 'react'
 export default class PostForm extends React.Component{
     constructor(props){
         super(props)
-        this.state = {}
+        this.state = {
+            title: ''
+        }
     }
 
     submitHandler = event => {
         event.preventDefault()
+
+        const {title} = this.state
+
+        const newPost = {
+            title, id: Date.now().toString()
+        }
+        console.log(this.state.title)
+        console.log(newPost)
+        this.setState({title: ''})
+    }
+
+    //Универсальный обработчик для всех input
+    changeInputHandler = event =>{      
+        event.persist() //позволит коду удерживать ссылки на это событие
+        this.setState(prev => ({...prev, ...{
+            [event.target.name]: event.target.value
+        }}))
     }
 
     render(){
@@ -18,7 +37,10 @@ export default class PostForm extends React.Component{
                     <input 
                         type="text" 
                         className="form-control" 
-                        id="title"/>
+                        id="title"
+                        value={this.state.title}
+                        name="title"
+                        onChange={this.changeInputHandler}/>
                 </div>
                 <button className='btn btn-success' type='submit'>Create</button>
             </form>
