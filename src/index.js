31bc9af8bbd9d08eaa62b5df05881461a7499b1c
Stas <nevmyvakastas@gmail.com>
2020-06-21 11:58:41 +0600

    created saga middleware

--- src/index.js
+++ src/index.js
@@ -3,20 +3,26 @@ import {render} from 'react-dom';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { rootReducer } from './redux/rootReducer';
 import {Provider} from 'react-redux'
+import createSagaMiddleware from 'redux-saga' 
 import thunk from 'redux-thunk'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { forbiddenWordsMiddleware } from './redux/middleware';
+import { sagaWatcher } from './redux/sagas';
 
 
+const saga = createSagaMiddleware()
 
 const store = createStore(rootReducer, compose(
   applyMiddleware(
     thunk,
-    forbiddenWordsMiddleware
+    forbiddenWordsMiddleware,
+    saga
   ),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
+
+saga.run(sagaWatcher)
 
 const app = (
   <Provider store={store}>
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,22 +1,14 @@
-import { CREATE_POST, FETCH_POST, SHOW_LOADER, HIDE_LOADER, SHOW_ALERT, HIDE_ALERT } from "./types";
+import { REQUEST_POSTS, CREATE_POST, FETCH_POST, SHOW_LOADER, HIDE_LOADER, SHOW_ALERT, HIDE_ALERT } from "./types";
 
 export const createPost = (post) =>({
     type: CREATE_POST,
     payload: post
 })
 
-export const fetchPosts = () => async dispatch => {
-    try{
-        dispatch(showLoader())
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-        const json = await response.json()
-        dispatch({type: FETCH_POST, payload: json})
-        dispatch(hideLoader())
-    }catch(e){
-        dispatch(showAlert('Something happend wrong!'))
-        dispatch(hideLoader())
+export const fetchPosts = () => {
+    return {
+        type: REQUEST_POSTS
     }
-    
 }
 
 export const showLoader = () => {
